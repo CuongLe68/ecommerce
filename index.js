4d9ad139ea215a1f2567ab8ea2d9dff8aa05c3ab
cuongw9 <cuongwf9@gmai.com>
2024-04-07 03:47:09 +0700

    test up vercel

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const cors = require("cors"); //giao tiếp giữa các page không bị chặn
 const cookieParser = require("cookie-parser"); //trung gian để phân tích cú pháp cookie
 const mongoose = require("mongoose"); //import db
 const dotenv = require("dotenv"); //cấu hình env
-
-//router
-const productRouter = require("./src/v1/routers/productRouter"); //liên quan đến product
+const port = process.env.PORT || 3000
 
 dotenv.config();
 app.use(cors());
@@ -23,9 +21,20 @@ mongoose
     console.error("Failed to connect to MongoDB", err);
   });
 
-//routers
-app.use("/v1/products", productRouter);
+// Page Home
+app.get("/", (req, res) => {
+  res.send('SERVER ON')
+})
 
-app.listen(8000, () => {
-  console.log(`SERVER IS RUNNING! http://localhost:8000`);
+//routers
+const productRouter = require("./src/v1/routers/productRouter"); //liên quan đến product
+app.use("/v1/products", cors({ origin: '*' }), productRouter);
+
+// Page Error
+app.get("*", (req, res) => {
+  res.send("Nhập Sai Đường Dẫn! Vui Lòng Nhập Lại >.<")
+});
+
+app.listen(port, () => {
+  console.log(`SERVER IS RUNNING! http://localhost:${port}`);
 });
